55e5421759246a8108a514e38c4b951e28746711
abel <afcal@berkeley.edu>
2021-03-02 01:23:41 +0000

    fix class description

--- portfolio/src/main/java/com/google/sps/servlets/StringServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/StringServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+/** Servlet that responds with "hello world" */
 @WebServlet("/string")
 public class StringServlet extends HttpServlet {
 
