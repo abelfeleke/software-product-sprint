64c8ef076fd3a06640e1a36f885cb9ddb98b8e21
abel <afcal@berkeley.edu>
2021-03-12 22:07:46 +0000

    Data Store project

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,10 +1,17 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
@@ -12,15 +19,19 @@ public class FormHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String email = request.getParameter("text-input");
+    // Get email entered in the form.
+    String email = Jsoup.clean(request.getParameter("text-input"), Whitelist.none());
 
-    // Print the value so you can see it in the server logs.
-    System.out.println(email);
+    // Create email entity
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Email");
+    FullEntity contactEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("email", email)
+            .build();
 
-    // Write the value to the response so the user can see it.
-    response.setContentType("text/html;");
-    response.getWriter().println(email);
-    response.sendRedirect("/form-handler");
+    // Store email in datastore
+    datastore.put(contactEntity);
+    response.sendRedirect("/index.html");
   }
 }
---
+++ portfolio/src/main/java/com/google/sps/servlets/ListEmailsServlet.java
@@ -0,0 +1,51 @@
+
+package com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.gson.Gson;
+import com.google.sps.data.Email;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet responsible for listing tasks. */
+@WebServlet("/list-emails")
+public class ListEmailsServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    Query<Entity> query =
+        Query.newEntityQueryBuilder().setKind("Email").build();
+
+    // Gets all enitites with kind "Email"
+    QueryResults<Entity> emailQuery = datastore.run(query);
+
+    List<Email> emails = new ArrayList<>();
+
+    // Load each email into emails ArrayList
+    while (emailQuery.hasNext()) {
+      Entity emailEntity = emailQuery.next();
+
+      long key = emailEntity.getKey().getId();
+      String email = emailEntity.getString("email");
+
+      Email task = new Email(key, email);
+      emails.add(task);
+    }
+
+    Gson gson = new Gson();
+
+    // Email entity list -> JSON 
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(emails));
+  }
+}
